f63991d5a2f7534a8bdd860218cc51d873d63345
umer wahid <umerhameed677@gmail.com>
2021-01-05 17:48:38 +0500

    all the remaining program added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let numbers = max(8,5);
-console.log(numbers);
+let numbertobeprint = max(8,5);
+console.log(numbertobeprint);
 function max(a,b) {
     if(a > b) return a;
     return b;
@@ -17,7 +17,7 @@ function isLandscap(width,height) {
    return(width > height)
 }
 
-excersize
+
 const num =fizzBuzz(15);
 console.log(num);
 function fizzBuzz(input) {
@@ -30,4 +30,82 @@ function fizzBuzz(input) {
 // here is 
     
 }
-let a =2;
+
+
+const value = checkspeed(190);
+
+function checkspeed(speed) {
+    const speedlimit = 70;
+    const kmperpoint = 5;
+    if(speed < 70)
+    console.log('ok');
+    else{
+        const point = Math.floor((speed - speedlimit) / kmperpoint);
+        if(point >= 12)
+        console.log('licensed suspended');
+        else
+        console.log('points',point)
+    }
+    
+}
+findNumbers(20);
+function findNumbers(limit) {
+    for(let i = 0; i<= limit; i++){
+        if (i % 2 === 0) console.log(i,'Even Number');
+        else console.log(i,'odd Numbers');
+        //const message = (i % 2 === 0) ? 'EVEN': 'ODD'
+        //console.log(i,message);
+
+    }
+    
+}
+
+console.log(sumofMultiples(110));
+function sumofMultiples(values) {
+    let sum = 0;
+
+    for(let i = 0; i<= values; i++)
+    if (i % 3 === 0 || i % 5 === 0)
+    sum += i;
+    return sum;
+    
+}
+
+
+const marks = [10,20,30];
+console.log(grad(marks));
+function grad(marks) {
+let sum =0;
+for(let mark of marks)
+sum += mark;
+let average = sum / marks.length;
+if(average < 50) return 'F';
+if(average < 60) return 'D';
+if(average < 70) return 'C';
+if(average <80) return 'B';
+if(average < 90) return 'A';
+return 'A+';
+
+
+    
+}
+stars(5);
+function stars(show){
+    for(let row = 1; row <= show; row++){
+        let pattern ='';
+        for (let i=0;i< row;i++)
+        pattern += '*';
+      console.log(pattern);
+    }
+}
+
+console.log(arrayRange(10,20));
+
+function arrayRange(min,max) {
+
+    const output =[];
+    for(let i = min; i<= max; i++)
+    output.push(i);
+    return output;
+    
+}
